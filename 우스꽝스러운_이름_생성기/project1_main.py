9604d7e61024a3e1f54d5ea363c3b0f7910858c2
luvMint <lovejd0815@gmail.com>
2022-12-04 20:14:49 +0900

    잘못된 상수명 에러처리 코드 지움

--- 우스꽝스러운_이름_생성기/project1_main.py
+++ 우스꽝스러운_이름_생성기/project1_main.py
@@ -1,33 +1,31 @@
 #우스광스러운 이름 생성기
 import sys, random
 
-def main():
 
-        print("Welcome th the Psych 'Sidekick Name Picker.'\n")
-        print("A name just like Sean would pick for Gus: \n\n")
 
-        first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'", "Bob 'Stinkbut'", 'Bowel Noises', 
+print("Welcome th the Psych 'Sidekick Name Picker.'\n")
+print("A name just like Sean would pick for Gus: \n\n")
+
+first = ('Baby Oil', 'Bad News', 'Big Burps', "Bill 'Beenie-Weenie'", "Bob 'Stinkbut'", 'Bowel Noises', 
         'Boxelder', "Bud 'Lite'")
 
-        last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom', 
+last = ('Appleyard', 'Bigmeat', 'Bloominshine', 'Boogerbottom', 
         'Breedslovetrout', 'Butterbaugh')
 
-        while True:
-                firstName = random.choice(first)
+while True:
+        firstName = random.choice(first)
 
-                lastName = random.choice(last)
+        lastName = random.choice(last)
 
-                print("\n\n")
-                print("{} {}".format(firstName, lastName), file=sys.stderr)
-                print("\n\n")
+        print("\n\n")
+        print("{} {}".format(firstName, lastName), file=sys.stderr)
+        print("\n\n")
 
-                try_again = input("\n\nTry again? (Press Enter else n to quit)\n")
-                if try_again.lower() == "n":
-                        break
+        try_again = input("\n\nTry again? (Press Enter else n to quit)\n")
+        if try_again.lower() == "n":
+                break
                 
-        input("\nPress Enter to exit.")
+input("\nPress Enter to exit.")
 
-if __name__ == "__main__":
-        main()
 
 
