b1b6e38a7f4e38517f39318c03d6d087b4522998
luvMint <lovejd0815@gmail.com>
2022-12-08 23:01:56 +0900

    docs : use arrangement -> use string index

--- 우스꽝스러운_이름_생성기_도전_프로젝트/pig_latin.py
+++ 우스꽝스러운_이름_생성기_도전_프로젝트/pig_latin.py
@@ -8,14 +8,18 @@ while True:
 
     word = input("type word you want to make piglatin: ")
 
-    #버그 수정 부분
-    for i in range(word):
-        if word[0] == vowel[i]:
+    i = 0
+    j = 1
+
+    for i in word:
+        #버그 수정 부분
+        if word[0:1] == vowel[i:j]:
             print("\n\n")
             print(word + "way")
             print("\n\n")
+            j += 1
 
-        elif word[0] != vowel[i]:
+        elif word[0:1] != vowel[i:j]:
             for n in range(vowel):
                 for j in range(word):
                     if word[0] == n:
@@ -23,7 +27,10 @@ while True:
                         print("\n\n")
                         print(pigLatin)
                         print("\n\n")
+            j += 1            
     #버그 수정 부분
+    
+    
     
     tryAgain = input("\n\nTry again? (Press Enter else 'quit' to quit)\n")
     if tryAgain.lower() == "quit":
