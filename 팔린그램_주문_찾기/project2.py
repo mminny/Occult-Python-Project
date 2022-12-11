bef34a0b89ef85063373762bc3279694317694c2
luvMint <lovejd0815@gmail.com>
2022-12-11 16:39:23 +0900

    feat : 파일 열 때의 예외 처리 기능 추가

--- 팔린그램_주문_찾기/project2.py
+++ 팔린그램_주문_찾기/project2.py
@@ -1,7 +1,29 @@
-try: 
-    with open(file) as in_file:
-        #do something
-except IOError as e:
-    print("{}\nError opening {}. Terminating program.".format(e, file),
-    file = sys.stderr)
-    sys.exit(1)
+"""
+Load a text file as a list.
+
+Arguments:
+-dictionary.txt
+
+Exceptions:
+-IOError if filename not found.
+
+Returns:
+-A list of all words in a text file in lower case.
+
+Requires:
+-import sys
+"""
+
+import sys
+
+def load(file):
+    """텍스트 파일을 열고 작은 순서대로 문자열을 배열로 구성한다."""
+    try:
+        with open(file) as in_file:
+            loaded_txt = in_file.read().strip().split('\n')
+            loaded_txt = [x.lower() for x in loaded_txt]
+            return loaded_txt
+
+    except IOError as e:
+        print("{}\nError opening {}. Terminating program.".format(e, file),
+                file=sys.stderr)
