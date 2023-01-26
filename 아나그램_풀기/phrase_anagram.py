4a1a43a47e4fa2c2058c09bc83e7de47d51458f4
luvMint <lovejd0815@gmail.com>
2023-01-26 23:26:13 +0900

    ✨ Add process_choice function

--- 아나그램_풀기/phrase_anagram.py
+++ 아나그램_풀기/phrase_anagram.py
@@ -42,8 +42,45 @@ def find_anagram(name, word_list):
         for letter in word:
             if word_letter_map[letter] <= name_letter_map[letter]:
                 test += letter
-                if Counter(test) == word_letter_map:
-                    anagrams.append(word)
+        if Counter(test) == word_letter_map:
+            anagrams.append(word)
+
+    print(*anagrams, sep='¥n')
+    print()
+    print("Remaining letters = {}".format(name))
+    print("Number of remaining letters = {}".format(len(name)))
+    print("Number of remaining (real word) anagrams = {}".format(len(anagrams)))
+
+def process_choice(name):
+    """Check uer choice for validity, return choice & leftover letters."""
+    while True:
+
+        choice = input('\nMake a choice else Enter to start over or # to end: ')
+
+        if choice == '':
+            main()
+        elif choice == '#':
+            sys.exit()
+        else:
+            candidate = ''.join(choice.lower().split())
+
+        left_over_list = list(name)
+        
+        for letter in candidate:
+            if letter in left_over_list:
+                left_over_list.remove(letter)
+        if len(name) - len(left_over_list) == len(candidate):
+            break
+        else:
+            print("Won' work! Mae another choice!", file=sys.stderr)
+
+    name = ''.join(left_over_list) # makes display mre readable
+    return choice, name
+
+def main():
+    
+
+
 
 
 
