a7e45f697617975f8cbb3666c7edbcbd27ab8f57
luvMint <lovejd0815@gmail.com>
2023-02-03 22:54:53 +0900

    🐛: fix possibly unbound error

--- 아나그램_풀기/phrase_anagram.py
+++ 아나그램_풀기/phrase_anagram.py
@@ -32,6 +32,7 @@ dict_file = sorted(dict_file)
 
 ini_name = input("Enter a name: ")
 
+
 def find_anagram(name, word_list):
     """Read name & dictionary file & display all anagram IN name"""
     name_letter_map = Counter(name)
@@ -53,18 +54,21 @@ def find_anagram(name, word_list):
 
 def process_choice(name):
     """Check uer choice for validity, return choice & leftover letters."""
+    
     while True:
 
         choice = input('\nMake a choice else Enter to start over or # to end: ')
+        
 
         if choice == '':
             main()
         elif choice == '#':
             sys.exit()
         else:
-            candidate = ''.join(choice.lower().split())
+            candidate = ''.join(choice.lower().split()) # .join(), .lower(), .split() 함수
 
         left_over_list = list(name)
+        candidate = ''
         
         for letter in candidate:
             if letter in left_over_list:
@@ -72,13 +76,48 @@ def process_choice(name):
         if len(name) - len(left_over_list) == len(candidate):
             break
         else:
-            print("Won' work! Mae another choice!", file=sys.stderr)
+            print("Won't work! Make another choice!", file=sys.stderr)
 
-    name = ''.join(left_over_list) # makes display mre readable
+    name = ''.join(left_over_list) # makes display more readable
     return choice, name
 
 def main():
-    
+    """Help user build anagram phrase fro their name."""
+    name = ''.join(ini_name.lower().split()) 
+    name = name.replace('-', '') # replace 함수
+
+    limit = len(name)
+    phrase = ''
+    running = True
+
+    while running:
+        temp_phrase = phrase.replace(' ', '')
+        if len(temp_phrase) < limit:
+            print("Length of anagram phrase = {}".format(len(temp_phrase)))
+
+            find_anagram(name, dict_file)
+            print("Current angram phrase =", end=" ") # print 함수의 end 매개변수
+            print(phrase, file=sys.stderr)
+
+            choice, name = process_choice(name)
+            phrase += choice + ' '
+
+        elif len(temp_phrase) == limit:
+            print("\n*****FNISHED!!!*****\n")
+            print("Anagram of name =", end=" ")
+            print(phrase, file=sys.stderr)
+            print()
+            try_again = input('\n\nTry Again? (Press Enter else "n" to quit)\n ')
+            if try_again.lower() == "n":
+                running = False
+                sys.exit()
+            else:
+                main()
+
+if __name__ == '__main__':
+    main()
+
+
 
 
 
